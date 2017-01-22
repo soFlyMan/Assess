c1c1b4a2b47bbc11afc86763310fbf8e28351041
soFly <1351370237@qq.com>
2017-01-22 10:11:40 +0800

    v0.0.4.2.1
    fetch user actions

--- component/admin/examAdmin/SinglePaper.js
+++ component/admin/examAdmin/SinglePaper.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react'
 
-class SinglePaper extends Component{
-	render(){
-		const { fetched, singlePaper } = this.props
-		if(fetched){
-		return (
+const SinglePaper = ({ fetched, singlePaper }) =>{
+	if(fetched){
+	return (
+		<div>
+			<h3>一、单项选择题</h3>
 			<div>
-				<h3>一、单项选择题</h3>
-				<div>
+			{
+				singlePaper
+				.radio
+					.map(val =>{
+						return (
+							<div>
+								{val.body}
+								{val.options}
+								{val.answer}
+							</div>
+								) 
+					}
+				)
+			}
+			</div>
+			<h3>二、多项选择题</h3>
+			<div>
 				{
 					singlePaper
-					.radio
-						.map(val =>{
-							return (
-								<div>
+					.multi
+						.map(val => {
+							return <div>
 									{val.body}
 									{val.options}
-								</div>
-									) 
-						}
-					)
+									{val.answer}
+								   </div>
+						})
 				}
-				</div>
-				<h3>二、多项选择题</h3>
-				<h3>三、填空题</h3>
-				<h3>四、改错题</h3>
-				<h3>五、判断题</h3>
-				<h3>六、编程题</h3>
 			</div>
-			)
-		}else{
-			return <div>正在加载中……</div>
-		}	
-	}
+			<h3>三、填空题</h3>
+			<h3>四、改错题</h3>
+			<h3>五、判断题</h3>
+			<h3>六、编程题</h3>
+		</div>
+		)
+	}else{
+		return <div>正在加载中……</div>
+	}	
 }
 
 export default SinglePaper
---
+++ redux/actions/userActions.js
@@ -0,0 +1,32 @@
+export const FETCH_USER = 'FETCH_USER'
+export const FETCHED_USER = 'FETCHED_USER'
+export const FETCH_USER_ERR = 'FETCH_USER_ERR'
+
+export const FETCH_SINGLE_USER = 'FETCH_SINGLE_USER'
+export const FETCHED_SINGLE_USER = 'FETCHED_SINGLE_USER'
+export const FETCHE_SINGLE_USER_ERR = 'FETCHE_SINGLE_USER_ERR'
+
+export fetchUser = (url,params) => {
+	return dispatch => {
+		return dispatch({ type: FETCH_USER })
+				fetch(url,params)
+					.then(res => res.json()
+						.then(data => {
+							dispatch({ type: FETCHED_USER, payload: data })
+						})).catch(err => {
+						dispatch({ type: FETCH_USER_ERR, payload: err})
+					})
+	}
+}
+
+export fetchSingleUser = (url,params) => {
+	return dispatch => {
+		return dispatch({ type: FETCH_SINGLE_USER })
+				fetch(url,params)
+					.then(res => res.json()
+						.then(data => {
+							dispatch({ type: FETCHED_SINGLE_USER, payload: data})
+						})).catch(err => {
+						dispatch({type: FETCHE_SINGLE_USER_ERR, payload: err})
+					})
+}
