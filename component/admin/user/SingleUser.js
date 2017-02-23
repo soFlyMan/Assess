0af5318f379a359b12877b41caa3cb35c7a5a3ef
soFly <1351370237@qq.com>
2017-02-23 12:10:06 +0800

    v0.0.5.0.4
    Singleuserpaper

--- component/admin/user/SingleUser.js
+++ component/admin/user/SingleUser.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Tabs } from 'antd'
+
+import SingleUserPaper from './SingleUserPaper.js'
+const TabPane = Tabs.TabPane
 
 const SingleUser = ({fetched,singleUser}) => {
 	if(fetched){
+		var a = 1
 		return (
 			<div>
-				{singleUser.class}
+				<ul>
+					<li>姓名：{singleUser.username}</li>
+					<li>学号：{singleUser.userid}</li>
+					<li>查看试卷：</li>
+					<li>
+						<Tabs type="card">
+						{
+							singleUser.exampaper.map(val => {
+								return (
+									<TabPane tab={val.date} key={a++}><SingleUserPaper paper={val}/></TabPane>
+								)
+							})
+						}
+						</Tabs>
+					</li>
+				</ul>
 			</div>
 			)
 	}else{
---
+++ component/admin/user/SingleUserPaper.js
@@ -0,0 +1,124 @@
+import React from 'react'
+
+const SingleUserPaper = ({paper}) => {
+	var a = 1
+	var b = 1
+	var c = 1
+	var d = 1
+	var e = 1
+	var f = 1
+	return (
+		<div>
+			<h3>一、单项选择题(每题{paper.radioScore}分)</h3>
+			<div>
+			{
+				paper
+				.radio
+					.map(val =>{
+						return (
+							<div>
+								<pre><span>{a++}.</span>
+								{val.body}</pre>
+								<pre>
+									{val.options}
+								</pre>
+								<pre>
+									<span className="answer">答案：{val.answer}</span>
+								</pre>
+								<br/>
+							</div>
+								) 
+					}
+				)
+			}
+			</div>
+			<h3>二、多项选择题(每题{paper.multiScore}分)</h3>
+			<div>
+				{
+					paper
+					.multi
+						.map(val => {
+							return <div>
+									<pre>
+						       		  <span>{b++}.</span>{val.body}
+							       	</pre>
+							        <pre>{val.options}</pre>
+							        <span className="answer">答案：
+							      	{val.answer}
+							        </span>
+								    <br/>
+								   </div>
+						})
+				}
+			</div>
+			<h3>三、填空题(每题{paper.fillblankScore}分)</h3>
+			{
+					paper
+					.fillblank
+						.map(val => {
+							return <div><pre>
+								       <span>{c++}.</span>{val.body}</pre>
+									   <pre>
+									      <span className="answer">答案：
+									      	{val.answer}
+									      </span>
+									   </pre>
+									   <br/>
+								   </div>
+						})
+				}
+			<h3>四、改错题(每题{paper.correctScore}分)</h3>
+			{
+					paper
+					.correct
+						.map(val => {
+							return <div><pre>
+								       <span>{d++}.</span>{val.body}</pre>
+									   <pre>
+									      <span className="answer">答案：
+									      	{val.answer}
+									      </span>
+									   </pre>
+									   <br/>
+								   </div>
+						})
+				}
+			<h3>五、判断题(每题{paper.judgeScore}分)</h3>
+			{
+					paper
+					.judge
+						.map(val => {
+							return <div>
+									  <pre>
+								       <span>{e++}.</span>
+								       {val.body}
+							          </pre>
+								      <span className="answer">答案：
+								      	{val.answer}
+								      </span>
+								      <br/>
+								   </div>
+						})
+				}
+			<h3>六、编程题(每题{paper.programmingScore}分)</h3>
+			{
+					paper
+					.programming
+						.map(val => {
+							return <div>
+								       <pre><span>{f++}.</span>
+								       {val.body}</pre>
+									   <pre>
+									      <span className="answer">答案：
+									      	{val.answer}
+									      </span>
+									   </pre>
+									   <br/>
+								   </div>
+						})
+				}
+		</div>
+		)	
+}
+
+export default SingleUserPaper
